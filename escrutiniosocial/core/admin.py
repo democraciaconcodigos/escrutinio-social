2ee2698d7dfdb31a4f16dd014731c0378d654d86
Joel Alejandro Villarreal Bertoldi <hola@joelalejandro.com>
2013-11-09 19:20:58 +0000

    Fixed nessita typo

--- escrutiniosocial/core/admin.py
+++ escrutiniosocial/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from core.models import Eleccion, Opcion
 
-#admin.register(Eleccion)
-#admin.register(Opcion)
+admin.site.register(Eleccion)
+admin.site.register(Opcion)
