b659bcf34828c21a8698368b87fd64f286175281
Martín Gaitán <gaitan@gmail.com>
2013-11-09 12:40:29 -0300

    add south

--- escrutiniosocial/escrutiniosocial/settings.py
+++ escrutiniosocial/escrutiniosocial/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external apps
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
