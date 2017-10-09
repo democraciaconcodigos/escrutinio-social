959860bcaf8078189d073f97c426603eba8aee1a
Martin Gallo <mgallo@coresecurity.com>
2017-10-09 20:39:31 -0300

    Levantando fixtures

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ if [ "$1" == 'init' ]; then
     echo "Inicializar base de datos"
     ${SITE_DIR}/env/bin/python ${SITE_DIR}/proj/manage.py migrate
     ${SITE_DIR}/env/bin/python ${SITE_DIR}/proj/manage.py collectstatic --no-input
+    ${SITE_DIR}/env/bin/python ${SITE_DIR}/proj/manage.py loaddata ${SITE_DIR}/proj/fixtures/*
 elif [ "$1" == 'manage' ]; then
     shift
     echo "Manage.py $@"
