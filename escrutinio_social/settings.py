271e882cb9443a883cbd48ce25b4a4eb5100e3e2
Martin Gallo <mgallo@coresecurity.com>
2017-10-10 19:51:07 -0300

    Deployment: Secret key #42
    Configurar el secret key de Django usando una variable de ambiente.

--- escrutinio_social/settings.py
+++ escrutinio_social/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'changeme'
+SECRET_KEY = env("DJANGO_SECRET_KEY", 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG", True)
